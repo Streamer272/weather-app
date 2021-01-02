eb6c4a9c618d9ca1129858705aa8b23fe0b56171
Streamer272 <dankosaurus9@gmail.com>
2021-01-02 16:51:14 +0100

    yeah its done

--- src/App.js
+++ src/App.js
@@ -11,11 +11,18 @@ const App = () => {
     setCity('');
   };
 
+  const getTemp = () => {
+    const r1 = Math.floor(Math.random() * 10);
+    const r2 = Math.floor(Math.random() * 10) / 10;
+    const result = r1 + r2;
+    return result;
+  };
+
   return (
     <div className="App">
       <input onChange={(e) => { setCity(e.target.value) }} value={ city }/>
       <button onClick={() => { addCity() }}>+</button>
-      { cities.map( (c) => { return <City cityName={ c } cityTemp={ 10 } /> }) }
+      { cities.map( (c) => { return <City cityName={ c } cityTemp={ getTemp() } /> }) }
     </div>
   );
 };
